45b1d7666787be77e47a81f7d58daf6e9d384164
Olivier Roussel <olivier.roussel@inria.fr>
2024-10-22 12:23:09 +0200

    Tester support has been removed since numpy 1.25

--- python/pymesh/__init__.py
+++ python/pymesh/__init__.py
@@ -2,9 +2,6 @@ from .version import __version__
 from . import PyMeshSetting
 from .timethis import timethis
 
-from numpy.testing import Tester
-test = Tester().test
-
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
 try:  # Python 2.7+
